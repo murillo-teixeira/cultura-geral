715e89be99bff69c148def23a949b9ab98db2222
Murillo Teixeira <murillo.teixeira@usp.br>
2023-11-27 17:49:35 +0000

    Changing small css and index.js details

--- pages/index.jsx
+++ pages/index.jsx
@@ -36,7 +36,7 @@ export default function Home({ sheetData }) {
   return (
     <div>
       <Head>
-        <title>Google Sheets Data in Next.js</title>
+        <title>Pontuação - Competição Geral</title>
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700;900&display=swap" rel="stylesheet"/>
